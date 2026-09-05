af8dae9c94e63b5d5a76aedbb4c42486e67b111d
Justinas Zvikas <jzvikas@gmail.com>
2026-09-05 13:29:11 +0300

    Bypass query cache for UPDATE transaction state

--- src/Import/UpdateStage.php
+++ src/Import/UpdateStage.php
@@ -162,7 +162,7 @@ final class UpdateStage
 
     private function transactionIsActive(\Db $db): bool
     {
-        $value = $db->getValue('SELECT @@session.in_transaction');
+        $value = $db->getValue('SELECT @@session.in_transaction', false);
         if ($value === false) { throw new \RuntimeException('Could not inspect UPDATE transaction state: ' . $db->getMsgError()); }
         return (int) $value === 1;
     }
