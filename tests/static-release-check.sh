5bea109bd344fc30db438eda0bbd775181fafe7a
Justinas Zvikas <jzvikas@gmail.com>
2026-09-05 11:16:03 +0300

    Include image worker contract in release checks

--- tests/static-release-check.sh
+++ tests/static-release-check.sh
@@ -14,4 +14,5 @@ php tests/import-orchestration-contract-check.php
 php tests/update-orchestration-contract-check.php
 php tests/remove-orchestration-contract-check.php
 php tests/image-pipeline-foundation-contract-check.php
+php tests/image-pipeline-worker-contract-check.php
 echo "Static release checks: OK"
