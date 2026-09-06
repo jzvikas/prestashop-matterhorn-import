b57dc5c05d3032db38d3a948e846760d46ba19ac
Justinas Zvikas <jzvikas@gmail.com>
2026-09-06 15:51:36 +0300

    Use Composer autoload in category path tests

--- tests/category-path-read-bound-check.php
+++ tests/category-path-read-bound-check.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once dirname(__DIR__) . '/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use Lp\MatterhornImport\Matterhorn\MatterhornCategoryPathNormalizer;
 
