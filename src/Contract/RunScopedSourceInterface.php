610b89452eb899729b65772e53f5647e519c22a2
Justinas Zvikas <jzvikas@gmail.com>
2026-09-06 23:27:13 +0300

    Simplify run-scoped source contract

--- src/Contract/RunScopedSourceInterface.php
+++ src/Contract/RunScopedSourceInterface.php
@@ -5,7 +5,5 @@ interface RunScopedSourceInterface
 {
     public function activateRun(int $runId, bool $resume): void;
 
-    public function persistRunCheckpoint(int $runId, int $recordCheckpoint, int $byteCheckpoint): void;
-
     public function releaseRun(int $runId): void;
 }
