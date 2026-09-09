41cf787be8285dcf0bd987a40e3b042fd5423e87
Justinas Zvikas <jzvikas@gmail.com>
2026-09-09 23:26:25 +0300

    Sync 1.0.11 module changes

--- matterhornimport.php
+++ matterhornimport.php
@@ -17,7 +17,7 @@ class MatterhornImport extends Module
     {
         $this->name = 'matterhornimport';
         $this->tab = 'administration';
-        $this->version = '0.1.8';
+        $this->version = '1.0.11';
         $this->author = 'LP';
         $this->need_instance = 0;
         $this->bootstrap = true;
